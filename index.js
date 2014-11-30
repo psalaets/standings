983166e95e7b40094ca4d5991c6209b36952b4ce
Paul Salaets <psalaets@gmail.com>
2014-11-29 20:30:10 -0500

    handle ties and give correct rank to thing after a tie

--- index.js
+++ index.js
@@ -8,7 +8,16 @@ function applyRanks(items, rankBy) {
     return b[rankBy] - a[rankBy];
   });
 
-  items.forEach(function(item, index) {
-    item.rank = index + 1;
+  items.forEach(function(item, index, items) {
+    var previousItem = items[index - 1];
+
+    // if item's rank value is same as prev item's rank value
+    if (previousItem && item[rankBy] === previousItem[rankBy]) {
+      // they're tied and have same rank
+      item.rank = previousItem.rank;
+    } else {
+      // item's rank is its one-based position in array
+      item.rank = index + 1;
+    }
   });
 }
--- test/test.js
+++ test/test.js
@@ -16,3 +16,43 @@ test('items with no ties', function(t) {
   t.equal(teams[1].rank, 3);
   t.equal(teams[2].rank, 1);
 });
+
+test('items with one tie', function(t) {
+  t.plan(3);
+
+  var teams = [
+    {score: 30},
+    {score: 10},
+    {score: 30}
+  ];
+
+  standings(teams, 'score');
+
+  t.equal(teams[0].rank, 1);
+  t.equal(teams[1].rank, 3);
+  t.equal(teams[2].rank, 1);
+});
+
+test('items with multiple ties', function(t) {
+  t.plan(7);
+
+  var teams = [
+    {score: 60}, // 1st tie
+    {score: 60}, // 1st tie
+    {score: 50},
+    {score: 40},
+    {score: 30}, // 2nd tie
+    {score: 30}, // 2nd tie
+    {score: 20}
+  ];
+
+  standings(teams, 'score');
+
+  t.equal(teams[0].rank, 1);
+  t.equal(teams[1].rank, 1);
+  t.equal(teams[2].rank, 3);
+  t.equal(teams[3].rank, 4);
+  t.equal(teams[4].rank, 5);
+  t.equal(teams[5].rank, 5);
+  t.equal(teams[6].rank, 7);
+});
